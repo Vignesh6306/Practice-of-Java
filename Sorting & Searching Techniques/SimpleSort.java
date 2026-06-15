e4dab12b484bc183e805a989566061ec45e93df2
Vignesh6306 <vigneshwaran0636@gmail.com>
2026-06-15 22:07:46 +0530

    The Practise codes for Java

---
+++ Sorting & Searching Techniques/SimpleSort.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class SimpleSort{
+    public static void Sort(int[] arr){
+        Arrays.sort(arr);
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Size : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the Element : ");
+        for(int i=0; i<n; i++)
+            arr[i] = sc.nextInt();
+
+        System.out.println("Before Sorting : "+ Arrays.toString(arr));
+        Sort(arr);
+        System.out.println("After Sorting : "+ Arrays.toString(arr));
+    }
+}
